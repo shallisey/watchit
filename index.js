65e01561fc2d3233c90fbde8967b1916ce69f652
shallisey <47544304+shallisey@users.noreply.github.com>
2020-03-31 10:12:09 -0500

    Install caporal, file system, child_process, and chalk packages from npm. Caporal for help generation for command line applications. File system to see if the file even exists with fs.promises.acces(). Child_process.spawn allows you to execute a filename within a directory. Chalk to allow different colors in the command line.

--- index.js
+++ index.js
@@ -2,16 +2,42 @@
 
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const chalk = require('chalk');
 
-const start = debounce(() => {
-  console.log('starting users program');
-}, 200);
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute.')
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
 
-chokidar
-  .watch('.')
-  //when a file is created or sees a file in a directory for the first time you start program
-  .on('add', start)
-  //when a file is changed in the driectory
-  .on('change', () => console.log('File changed'))
-  //when a file is removed or "unlinked" from directory
-  .on('unlink', () => console.log('File unlinked'));
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.blue('>>>>> Starting process....'));
+      proc = spawn('node', [name], { stdio: 'inherit' });
+    }, 100);
+
+    chokidar
+      .watch('.', {
+        //
+        ignored: /\.git.|node_modules/
+      })
+      //when a file is created or sees a file in a directory for the first time you start program
+      .on('add', start)
+      //when a file is changed in the directory
+      .on('change', start)
+      //when a file is removed or "unlinked" from directory
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
