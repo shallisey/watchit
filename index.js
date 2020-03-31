f637a463bab2ed313f43479e7c7d9b3827fd1fb1
shallisey <47544304+shallisey@users.noreply.github.com>
2020-03-31 08:37:46 -0500

    Setup chokidar for add, change, and unlink. Install lodash.debounce and setup a debounce funtion so the .add is not starting and restarting the program for every single file every time the program is executed.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log('executed');
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+
+const start = debounce(() => {
+  console.log('starting users program');
+}, 200);
+
+chokidar
+  .watch('.')
+  //when a file is created or sees a file in a directory for the first time you start program
+  .on('add', start)
+  //when a file is changed in the driectory
+  .on('change', () => console.log('File changed'))
+  //when a file is removed or "unlinked" from directory
+  .on('unlink', () => console.log('File unlinked'));
